7058b603e37d0359e78cea4748483aea5077625a
tpauvreau <tony.pauvreau@monext.net>
2020-02-13 09:38:06 +0100

    lecture sans charger la mem

--- src/main/java/fr/nohlan/open/largefile/SampleReader2.java
+++ src/main/java/fr/nohlan/open/largefile/SampleReader2.java
@@ -1,19 +1,18 @@
 package fr.nohlan.open.largefile;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
-public class SampleReader {
+public class SampleReader2 {
 
     public static void main(final String[] args) throws IOException {
 
         try (
                 RandomAccessFile reader = new RandomAccessFile("target/tmp.txt", "r");
                 FileChannel channel = reader.getChannel();
-                ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                ) {
 
             int bufferSize = 1024;
             if (bufferSize > channel.size()) {
@@ -21,13 +20,13 @@ public class SampleReader {
             }
             final ByteBuffer buff = ByteBuffer.allocate(bufferSize);
 
+            long fileContentLength = 0;
             while (channel.read(buff) > 0) {
-                out.write(buff.array(), 0, buff.position());
+                fileContentLength = fileContentLength + buff.position();
                 buff.clear();
             }
 
-            // final String fileContent = new String(out.toByteArray(), StandardCharsets.UTF_8);
-            final long sz = out.size();
+            final long sz = fileContentLength;
             System.out.format("%s B%n", sz);
             System.out.format("%s KB%n", sz/1024);
             System.out.format("%s MB%n", sz/1024/1024);
